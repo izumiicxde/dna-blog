aa9a6474f974197f07d7b99ea07a02cb0935710f
izumii <154342465+izumiCode@users.noreply.github.com>
2025-03-29 15:23:20 +0530

    just small patch

--- app/components/editor.tsx
+++ app/components/editor.tsx
@@ -43,8 +43,61 @@ const extensions = [
 ];
 
 const Editor = () => {
-  const { setEditor } = useEditorStore();
+  const { editor, setEditor } = useEditorStore();
   const { setContent, content } = useEditorContentStore();
+
+  function handleImageDrop(
+    view: EditorView,
+    event: DragEvent,
+    slice: Slice,
+    moved: boolean
+  ): boolean {
+    if (
+      !moved &&
+      event.dataTransfer &&
+      event.dataTransfer.files &&
+      event.dataTransfer.files[0]
+    ) {
+      let file = event.dataTransfer.files[0];
+      let filesize = (file.size / 1024 / 1024).toFixed(4);
+
+      if (
+        (file.type === "image/jpeg" || file.type === "image/png") &&
+        parseFloat(filesize) < 10
+      ) {
+        let _URL = window.URL || window.webkitURL;
+        let img = document.createElement("img"); // Fixes the `new Image()` error
+        img.src = _URL.createObjectURL(file);
+
+        img.onload = function () {
+          if (img.width > 5000 || img.height > 5000) {
+            toast(
+              "image needs to be less than 5000 pixels in height and width"
+            );
+          } else {
+            //TODO: upload the image to the server.
+            toast("valid image uploaded");
+            editor?.chain().focus().setImage({ src: img.src }).run();
+            // uploadImage(file)
+            //   .then((response) => {
+            //     console.log("Image uploaded:", response);
+            //     // Insert the image URL into the editor here
+            //   })
+            //   .catch((error) => {
+            //     window.alert(
+            //       "There was a problem uploading your image, please try again."
+            //     );
+            //   });
+          }
+        };
+      } else {
+        toast("Image need to be in jpg or png format and less than 10MB.");
+      }
+      return true;
+    }
+    return false;
+  }
+
   return (
     <>
       <EditorProvider
@@ -74,52 +127,3 @@ const Editor = () => {
 };
 
 export default Editor;
-
-function handleImageDrop(
-  view: EditorView,
-  event: DragEvent,
-  slice: Slice,
-  moved: boolean
-): boolean {
-  if (
-    !moved &&
-    event.dataTransfer &&
-    event.dataTransfer.files &&
-    event.dataTransfer.files[0]
-  ) {
-    let file = event.dataTransfer.files[0];
-    let filesize = (file.size / 1024 / 1024).toFixed(4);
-
-    if (
-      (file.type === "image/jpeg" || file.type === "image/png") &&
-      parseFloat(filesize) < 10
-    ) {
-      let _URL = window.URL || window.webkitURL;
-      let img = document.createElement("img"); // Fixes the `new Image()` error
-      img.src = _URL.createObjectURL(file);
-
-      img.onload = function () {
-        if (img.width > 5000 || img.height > 5000) {
-          toast("image needs to be less than 5000 pixels in height and width");
-        } else {
-          // upload the image to the server.
-          toast("valid image uploaded");
-          // uploadImage(file)
-          //   .then((response) => {
-          //     console.log("Image uploaded:", response);
-          //     // Insert the image URL into the editor here
-          //   })
-          //   .catch((error) => {
-          //     window.alert(
-          //       "There was a problem uploading your image, please try again."
-          //     );
-          //   });
-        }
-      };
-    } else {
-      toast("Image need to be in jpg or png format and less than 10MB.");
-    }
-    return true;
-  }
-  return false;
-}
